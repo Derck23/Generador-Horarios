72d287db0cd48345f9d471d8a1674c31ddbf2355
Derck23 <eramireznieves25@gmail.com>
2025-06-29 21:47:04 -0600

    1

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import '../styles/global.css';
-import '../styles/login.css';
-import logo from '../assets/logohorarios_1.png';
-import agenda from '../assets/calendar.png';
-import React, { useState } from 'react';
+import './styles/global.css';
+import './styles/login.css';
+import logo from './assets/logohorarios_1.png';
+import agenda from './assets/calendar.png';
+import { useState } from 'react';
 import './Login.css';
-import logo from '../../assets/logohorarios_1.png';
-import agenda from '../../assets/agenda.png';
+//import logohorarios_1 from '../assets/logohorarios_1.png';
 import { Link} from 'react-router-dom';
-import { login } from '../../services/authService';
+import { login } from '../services/authService';
 
 const Login = () => {
+
   const [form, setForm] = useState({ username: '', password: '' });
   const [error, setError] = useState('');
 
@@ -34,37 +34,30 @@ const Login = () => {
   };
 
   return (
-    <div className="login-container">
-      <div className="login-card">
-        <div className="login-left">
-        <div className="login-logo-wrapper">
-          <img src={logo} alt="Logo" className="login-logo" />
-        </div>jsx
-        <div className="login-form-wrapper">
-          <form className="login-form">
-            <input type="email" placeholder="Correo: " />
-            <input type="password" placeholder="Contraseña: " />
-            <Link to="/Home"><button type="submit" >ENTRAR</button></Link>
-        <div className="login-content">
-          <form className="login-form" onSubmit={handleSubmit}>
-            <input type="username" name='username' placeholder="username" value={form.username} onChange={handleChange} required />
-            <input type="password" name="password" placeholder="Contraseña:" value={form.password} onChange={handleChange} required />
-            {/*<Link to="/Home"></Link>*/}<button type="submit" >ENTRAR</button>
-            {error && <p className="error">{error}</p>}
-          </form>
+      <div className="login-container">
+        <div className="login-card">
+          <div className="login-left">
+          <div className="login-logo-wrapper">
+            <img src={logo} alt="Logo" className="login-logo" />
           </div>
           <div className="login-content">
-          <p className="register-text">
-            ¿No tienes cuenta? <a href="/Registro">Registrarme</a>
-          </p>
-        </div>
-        </div>
-        <div className="login-right">
-          <img src={agenda} alt="Ilustración" className="login-image" />
+            <form className="login-form" onSubmit={handleSubmit}>
+              <input type="username" name='username' placeholder="username" value={form.username} onChange={handleChange} required />
+              <input type="password" name="password" placeholder="Contraseña:" value={form.password} onChange={handleChange} required />
+              {/*<Link to="/Home"></Link>*/}<button type="submit" >ENTRAR</button>
+              {error && <p className="error">{error}</p>}
+            </form>
+            <p className="register-text">
+              ¿No tienes cuenta? <a href="/Registro">Registrarme</a>
+            </p>
+          </div>
+          </div>
+          <div className="login-right">
+            <img src={agenda} alt="Ilustración" className="login-image" />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
 };
 
 export default Login;
