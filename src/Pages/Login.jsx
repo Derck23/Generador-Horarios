4f889e230721849f2f8ac90d75a76b1084840494
Derck23 <eramireznieves25@gmail.com>
2025-06-30 19:11:52 -0600

    service Login y Register

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -3,7 +3,7 @@ import '../styles/global.css';
 import '../styles/login.css';
 import logo from '../assets/logohorarios_1.png';
 import agenda from '../assets/calendar.png';
-import { Link, useNavigate} from 'react-router-dom';
+import { Link} from 'react-router-dom';
 import { login } from '../services/authService';
 
 const Login = () => {
